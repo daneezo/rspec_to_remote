a43aad737bc1eb9bca60de776c3d0f8970e636ce
Daneez Owais <daneezo@gmail.com>
2016-10-30 18:13:44 -0400

    init

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,41 +1,58 @@
 require './spec/spec_helper'
 feature "Sign in page for MDLIVE" do
 
+  def login
+    visit ('/')
+    find("a.showlogin").click
+
+    fill_in 'login[email]', :with => 'demopatient5'
+    fill_in 'login[password]', :with => 'mdlive123'
+
+    click_button "Sign In"
+    expect(page).to have_content("My Health")
+  end
+
+  def sign_in_for_video_appointment
+
+    select "FLORIDA", :from => 'available_state'
+    click_button "confirmUserLocationButton"
+    # sleep 2
+    expect(page).to have_content("See a Doctor")
+    click_link "See a Doctor"
+    expect(page).to have_content("Choose First Available")
+    click_button "Choose First Available"
+    find('.video').click
+    click_button "Continue"
+    fill_in 'chief_complaint', :with => 'Sore Throat'
+    sleep 2
+    click_button "Continue"
+    all('input[value=no]').map(&:choose)
+    # page.execute_script('$(".control i.continue").trigger("click")')
+    # page.execute_script('$(".control i.continue").trigger("click")')
+    # page.execute_script('$(".control i.Urgent Care").trigger("click")')
+    # click_button
+    click_link 'Continue'
+    click_link 'Continue'
+    click_link ('btnOk')
+    find('.chosen-single').click
+    find('.chosen-drop').all('li').each{|li| li.click if li.text == 'Urgent Care'}
+    check 'terms_of_use'
+    check 'health_summary'
+    fill_in 'initials', :with => 'dd'
+    find(:xpath, '//input[@value="Create Appointment"]').click
+  end
+
+
   context "A valid account" do
 
-    it 'should be allowed to login' do
-      visit ''
-
-      find("a.showlogin").click
-
-      fill_in 'login[email]', :with => 'demopatient4'
-      fill_in 'login[password]', :with => 'mdlive123'
-
-      click_button "Sign In"
-      expect(page).to have_content("My Health")
+    before(:each) do
+      login
     end
 
-    it 'should have the correct content on the dashboard' do
-      visit ''
-
-      find("a.showlogin").click
-
-      fill_in 'login[email]', :with => 'sif123'
-      fill_in 'login[password]', :with => 'mdlive789'
-
-      click_button "Sign In"
-      expect(page).to have_content("See a Doctor")
-      expect(page).to have_content("your Health")
+    it 'should be allowed to have OCWR functionality' do
+     sign_in_for_video_appointment
+  #    # wait_for_content('', 5)
     end
-  end
-  # it 'should not let me in with an invalid account' do
-  #   visit 'users/sign_in'
-  #
-  #   fill_in 'user[email]', :with => 'a@a.com'
-  #   fill_in 'user[password]', :with => 'asdfasdf'
-  #
-  #   click_button "Log in"
-  #   expect(page).to have_content("Invalid email or password")
-  # end
 
+end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,11 @@ require 'capybara/rspec'
 require 'rubygems'
 require 'bundler/setup'
 require 'selenium-webdriver'
+# require './spec/features/search_spec.rb'
+require 'pry'
 
 Capybara.default_driver = :selenium
-Capybara.app_host = 'https://stage.mdlive.com/'
+Capybara.app_host = 'https://mdlqa3-www.mdlive.com/'
 Capybara.run_server = false
 
 Capybara.register_driver :selenium do |app|
@@ -13,4 +15,18 @@ Capybara.register_driver :selenium do |app|
       browser: :firefox,
       desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
   )
+end
+
+
+def wait_for_content(content, seconds)
+  for i in 0..seconds
+    if !has_content?(content, wait: seconds)
+      puts "Waiting content '#{content}' to appear #{i} seconds"
+      pause_for 1
+    else
+      puts "It took #{i} seconds to find '#{content}'"
+      return true
+    end
+  end
+  false
 end
